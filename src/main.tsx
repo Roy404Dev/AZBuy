471edbdad3e4a1d08f8243478b9623abac59d42a
Roy <rojusbalciunas@gmail.com>
2024-01-18 23:04:40 +0200

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import App from "./App.tsx";
 import { Auth0Provider } from "@auth0/auth0-react";
 
 const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
-const domain = import.meta.env.VITE_AUTH0_BASE_URLL;
+const domain = import.meta.env.VITE_AUTH0_BASE_URL;
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
