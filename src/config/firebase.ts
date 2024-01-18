f527cc4e3a05eb126a4b3b309f00a9ee8f172442
Roy <rojusbalciunas@gmail.com>
2024-01-18 23:02:20 +0200

    fix

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -6,7 +6,7 @@ import { getFirestore } from "firebase/firestore";
 
 //TODO HERE
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
+  apiKey: import.meta.env.FIREBASE_API_KEY,
   authDomain: "azbuy-42a89.firebaseapp.com",
   projectId: "azbuy-42a89",
   storageBucket: "azbuy-42a89.appspot.com",
--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { Auth0Provider } from "@auth0/auth0-react";
 
-const clientId = import.meta.env.AUTH0_CLIENT_ID;
-const domain = import.meta.env.AUTH0_BASE_URL;
+const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
+const domain = import.meta.env.VITE_AUTH0_BASE_URLL;
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Auth0Provider
-      domain={clientId && clientId}
-      clientId={domain && domain}
+      domain={clientId}
+      clientId={domain}
       authorizationParams={{
         redirect_uri: window.location.origin,
       }}
