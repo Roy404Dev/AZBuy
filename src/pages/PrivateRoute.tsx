869470d26e007b59fe0664c76a69a7e8af37c990
Roy <rojusbalciunas@gmail.com>
2024-01-18 19:30:53 +0200

    .

--- src/pages/PrivateRoute.tsx
+++ src/pages/PrivateRoute.tsx
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from "react";
-import { useAuth0 } from "@auth0/auth0-react";
-import { Navigate } from "react-router-dom";
+import React from "react";
+// import { useAuth0 } from "@auth0/auth0-react";
+// import { Navigate } from "react-router-dom";
 
 type ChildrenProp = {
   children: React.ReactNode;
