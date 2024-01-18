2ac5f91542b9f38c2835c4cc64ee1755e475a490
Roy <rojusbalciunas@gmail.com>
2024-01-18 14:07:24 +0200

    feat: modified and added dotenv

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { Auth0Provider } from "@auth0/auth0-react";
+
+const clientId = process.env.AUTH0_CLIENT_ID || "";
+const domain = process.env.AUTH0_BASE_URL || "";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Auth0Provider
-      domain="tsgroy.eu.auth0.com"
-      clientId="dnvrDGOv2BOf8gxPmgXX5bHUiEHj86VC"
+      domain={domain}
+      clientId={clientId}
       authorizationParams={{
         redirect_uri: window.location.origin,
       }}
