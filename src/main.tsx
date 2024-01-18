0d48fa5a4ba2a2545dbdff51c42748055597e50d
Roy <rojusbalciunas@gmail.com>
2024-01-18 22:52:58 +0200

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { Auth0Provider } from "@auth0/auth0-react";
 
+const clientId = import.meta.env.AUTH0_CLIENT_ID;
+const domain = import.meta.env.AUTH0_BASE_URL;
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Auth0Provider
       domain={import.meta.env.AUTH0_BASE_URL}
-      clientId={import.meta.env.AUTH0_CLIENT_ID}
+      clientId={domain && domain}
       authorizationParams={{
         redirect_uri: window.location.origin,
       }}
