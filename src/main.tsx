b83d788483df46bfc37c558f1926b01e5538432a
Roy <rojusbalciunas@gmail.com>
2024-01-18 21:52:01 +0200

    pls

--- src/main.tsx
+++ src/main.tsx
@@ -8,25 +8,14 @@ const domain = process.env.AUTH0_BASE_URL || "";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    {clientId ? (
-          <Auth0Provider
-          domain={clientId}
-          clientId={domain}
-          authorizationParams={{
-            redirect_uri: window.location.origin,
-          }}
-        >
-          <App />
-        </Auth0Provider>
-    ) : null}
-    {/* <Auth0Provider
-      domain={undefined}
-      clientId={undefined}
+    <Auth0Provider
+      domain={domain}
+      clientId={clientId}
       authorizationParams={{
         redirect_uri: window.location.origin,
       }}
     >
       <App />
-    </Auth0Provider> */}
+    </Auth0Provider>
   </React.StrictMode>
 );
