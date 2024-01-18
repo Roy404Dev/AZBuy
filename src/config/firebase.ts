5c0d412a8fff387d14ebe94e13ea11b60b848e62
Roy <rojusbalciunas@gmail.com>
2024-01-18 23:17:20 +0200

    fix

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -6,13 +6,13 @@ import { getFirestore } from "firebase/firestore";
 
 //TODO HERE
 const firebaseConfig = {
-  apiKey: import.meta.env.FIREBASE_API_KEY,
-  authDomain: "azbuy-42a89.firebaseapp.com",
-  projectId: "azbuy-42a89",
-  storageBucket: "azbuy-42a89.appspot.com",
-  messagingSenderId: "325149385418",
-  appId: "1:325149385418:web:1fafc13a8eba4ec2580a33",
-  measurementId: "G-LCZP3806J8",
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId:  process.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket:  process.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId:  process.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId:  process.env.VITE_FIREBASE_APP_ID,
+  measurementId:  process.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 
