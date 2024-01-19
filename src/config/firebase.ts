2aa518f3078bb2966d99a3d9aa9a67baa113774f
Roy <rojusbalciunas@gmail.com>
2024-01-19 10:34:07 +0200

    modified: env

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -6,7 +6,7 @@ import { getFirestore } from "firebase/firestore";
 
 //TODO HERE
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
+  apiKey: process.env.VITE_FIREBASE_API_KEY,
   authDomain: process.env.VITE_FIREBASE_AUTH_DOMAIN,
   projectId:  process.env.VITE_FIREBASE_PROJECT_ID,
   storageBucket:  process.env.VITE_FIREBASE_STORAGE_BUCKET,
